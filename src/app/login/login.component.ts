f9099ef7f1e6d98644af0fda2e9513ce0afc80f0
Andriy Markus <andr98m@gmail.com>
2018-08-06 18:09:08 +0300

    get token functionality added

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginErrorsMessage:string=' ';
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  SendLoginAndPassword(username:string, password:string){
+    var tokenurl = "/api/Token";
+    var requestbody = new URLSearchParams();
+    requestbody.set('username', username);
+    requestbody.set('password', password);
+    requestbody.set('grant_type', "password");
+    this.http.post<any>(tokenurl, requestbody.toString(), { headers: {"Content-Type": "application/x-www-form-urlencoded"}}).pipe( 
+      catchError((e:any) => {
+        if(e.error.hasOwnProperty("error") && e.error.error == "invalid_grant"){
+          this.loginErrorsMessage = "Login or password is incorrect";
+        } else{
+          this.loginErrorsMessage = "Cann't login, try again later";
+        }
+        return of(null);
+      })
+    ).subscribe(response => {
+      if(response != null && response.hasOwnProperty("access_token")){
+          sessionStorage.setItem("access_token", response.access_token);
+          this.loginErrorsMessage = " "
+      }
+     });
+  }
+
+}
